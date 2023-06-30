a7d2c835797cef551c18d0c67f4ca402785176f4
Véronie R. Cazeau <veronie.cazeau@gmail.com>
2023-06-30 02:44:55 -0400

    UPDATE episode 1

--- src/pages/VisualOne.tsx
+++ src/pages/VisualOne.tsx
@@ -1,63 +1,90 @@
 import { Container, Row, Col, Image } from '../utilis/Bootstrap'
+import { useEffect, useState } from 'react';
 import AppLayout from '../layout/AppLayout';
 
-const Hompage = () => {
 
-    const imageUri = process.env.PUBLIC_URL + '/images/VN1';
-
-    return(
-        <AppLayout>
-        <section>
-        <Container fluid className= "visual-board">
-
-            <Row className="g-0">
-                <Col lg={12}>
-                <Image src= { imageUri + "/look.webp"}/>
-                </Col>
-            </Row>
-            <Row className="justify-content-center">
-            <Col lg={7}>
-            <Container className= "vn1-images">
-              <Row className="g-0">
-                <Col lg={12}><Image src={imageUri + "/hello-name.webp"} /></Col>
-              </Row>
-
-              <Row className="g-0">
-                <Col lg={12}><Image src={imageUri + "/uhm.webp"} /></Col>
-              <Row>
-
-              </Row>
-                <Col lg={12}><Image src={imageUri + "/please-four.webp"} /></Col>
-              </Row>
-
-              <Row className="g-0">
-                <Col lg={12}><Image src={imageUri + "/please-three.webp"} /></Col>
-                <Col lg={12}><Image src={imageUri + "/please-two.webp"} /></Col>
-              </Row>
-
-              <Row className="g-0">
-                <Col>
-                  <Image src={imageUri + "/please-one.webp"} />
-                </Col>
-              </Row>
-            </Container>            
-            </Col>
-            </Row>
-            <Row className= "vn1-caption">
-                <Col>
-                <p>I beg your pardon.
-                    <br/>
-                    Could you repeat what you just said? 
-                    <br/>
-                    I beg your pardon.
-                </p>
-                </Col>
-            </Row>
-
-        </Container>
-        </section>
-        </AppLayout>
-    )
+interface ImageData {
+  _id: string;
+  name: string;
+  image: string;
 }
 
-export default Hompage;
+const VisualOne = () => {
+  const [imageData, setImageData] = useState<ImageData[]>([]);
+
+  useEffect(() => {
+    fetch('https://graphic-novel-js-api.web.app/visual-story-one')
+      .then(response => response.json())
+      .then(data => {
+        setImageData(data);
+      })
+      .catch(error => {
+        console.error('Error fetching image data:', error);
+      });
+  }, []);
+
+  return (
+    <AppLayout>
+      <section>
+        <Container fluid className="visual-board">
+          <Row className="g-0">
+            <Col lg={12}>
+              {imageData.length > 0 && <Image src={imageData[6]?.image} />}
+            </Col>
+          </Row>
+          <Row className="justify-content-center">
+            <Col lg={7}>
+              <Container className="vn1-images">
+                <Row className="g-0">
+                  <Col lg={12}>
+                    {imageData.length > 0 && <Image src={imageData[5]?.image} />}
+                  </Col>
+                </Row>
+
+                <Row className="g-0">
+                  <Col lg={12}>
+                    {imageData.length > 0 && <Image src={imageData[4]?.image} />}
+                  </Col>
+                </Row>
+
+                <Row className="g-0">
+                  <Col lg={12}>
+                    {imageData.length > 0 && <Image src={imageData[3]?.image} />}
+                  </Col>
+                </Row>
+
+                <Row className="g-0">
+                  <Col lg={12}>
+                    {imageData.length > 0 && <Image src={imageData[2]?.image} />}
+                  </Col>
+                  <Col lg={12}>
+                    {imageData.length > 0 && <Image src={imageData[1]?.image} />}
+                  </Col>
+                </Row>
+
+                <Row className="g-0">
+                  <Col>
+                    {imageData.length > 0 && <Image src={imageData[0]?.image} />}
+                  </Col>
+                </Row>
+              </Container>
+            </Col>
+          </Row>
+          <Row className="vn1-caption">
+            <Col>
+              <p>
+                I beg your pardon.
+                <br />
+                Could you repeat what you just said?
+                <br />
+                I beg your pardon.
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </AppLayout>
+  );
+};
+
+export default VisualOne;
