dbe0a4266b110215f2e12215d003a2cfbcdad3cb
Véronie R. Cazeau <veronie.cazeau@gmail.com>
2023-07-10 23:57:37 -0400

    ADDED visual

--- src/pages/VisualFour.tsx
+++ src/pages/VisualFour.tsx
@@ -6,19 +6,49 @@ const VisualFour = () => {
         <AppLayout>
             <section>
             <Container>
-            <h2 style={{ color: 'white', textAlign: 'center' }}>Futuristic Harmony: the Intersection of Nature and Software Engineering</h2>
+            <h2 style={{ color: 'white', textAlign: 'center' }}>Futuristic Harmony: Nature and Software Engineering</h2>
                 <Row>
-                    <Col>
-                     {/* insert video 1:1 */}
+                    <Col className='robo'>
+                        <iframe 
+                            width="328" 
+                            height="583" 
+                            src="https://www.youtube.com/embed/FQim7RRinz8" 
+                            title="Robo-Organics" 
+                            frameBorder="0" 
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                            allowFullScreen></iframe>
                     </Col>
-                    <Col>
+                    <Col className='flora'>
+                        <iframe 
+                            width="328"
+                            height="583" 
+                            src="https://www.youtube.com/embed/LTLToKkSfN8" 
+                            title="Tomorrow&#39;s Flora" 
+                            frameBorder="0" 
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                            allowFullScreen></iframe>
                     </Col>
-                    <Col>
+                    <Col className='tech-ture'>
+                        <iframe 
+                            width="328" 
+                            height="583" 
+                            src="https://www.youtube.com/embed/7C11vifHAKk" 
+                            title="Tech-ture" 
+                            frameBorder="0" 
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                            allowFullScreen></iframe>
                     </Col>
                 </Row>
                 <Row>
-                    <Col>
-                    {/* insert mobile*/}
+                    <Col className='tech-garden'>
+                        <iframe 
+                            width="328" 
+                            height="583" 
+                            src="https://www.youtube.com/embed/zaSr7ZeD_dw" 
+                            title="Tech Garden" 
+                            frameBorder="0" 
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                            allowFullScreen></iframe>
                     </Col>
                 </Row>
             </Container>
